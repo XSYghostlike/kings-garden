7feb22de8e58d27f58a95e2b005ee042cdee99b8
NecroSheep <xiashiyang1993@gmail.com>
2018-07-23 19:07:49 -0700

    Utilize ParameterizedTest

--- tst/Q1Test.java
+++ tst/Q1Test.java
@@ -1,5 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -12,11 +17,18 @@ class Q1Test {
         q1 = new Q1();
     }
 
-    @Test
-    void tests() {
-        assertArrayEquals(new int[]{2,4}, q1.twoSum(new int[]{3,4,5,6,7}, 12));
-        assertArrayEquals(new int[]{3,4}, q1.twoSum(new int[]{3,4,5,6,7}, 13));
-        assertArrayEquals(new int[]{0,1}, q1.twoSum(new int[]{3,4,5,6,7}, 7));
-        assertArrayEquals(new int[]{0,1}, q1.twoSum(new int[]{3,3,4}, 6));
+    @ParameterizedTest
+    @MethodSource("argsProvider")
+    void tests(int[] expected, int[] arr, int target) {
+        assertArrayEquals(expected, q1.twoSum(arr, target));
+    }
+
+    static private Stream<Arguments> argsProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{2,4}, new int[]{3,4,5,6,7}, 12),
+                Arguments.of(new int[]{3,4}, new int[]{3,4,5,6,7}, 13),
+                Arguments.of(new int[]{0,1}, new int[]{3,4,5,6,7}, 7),
+                Arguments.of(new int[]{0,1}, new int[]{3,3,4}, 6)
+        );
     }
 }
--- tst/Q9Test.java
+++ tst/Q9Test.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
@@ -12,13 +14,15 @@ class Q9Test {
         q9 = new Q9();
     }
 
-    @Test
-    void tests() {
-        assertTrue(q9.isPalindrome(121));
-        assertFalse( q9.isPalindrome(-121));
-        assertTrue(q9.isPalindrome(0));
-        assertTrue(q9.isPalindrome(-0));
-        assertTrue(q9.isPalindrome(1));
-        assertFalse(q9.isPalindrome(10));
+    @ParameterizedTest
+    @ValueSource(ints = {121, 0, -0, 1})
+    void positiveTests(int x) {
+        assertTrue(q9.isPalindrome(x));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-121, 10})
+    void negativeTests(int x) {
+        assertFalse(q9.isPalindrome(x));
     }
 }
